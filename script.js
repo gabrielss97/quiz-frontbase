4013a1d48ee2b42005a657e1b5d38dd42a45302f
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-10-26 20:31:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const quizData = [
+  {
+    title: `Acrescente uma "tooltip" ao parágrafo abaixo com o texto "Sobre Quizcode".`,
+    question: `<p ***="Sobre Quizcode">Quizcode de quiz sobre programação.</p>`,
+    a: "h1",
+    b: "id",
+    c: "class",
+    d: "title",
+    correct: "d",
+  },
+  {
+    title: `Ajuste o tamanho da imagem para 250 pixels de largura e 400 pixels de altura.`,
+    question: '<img src="w3schools.jpg" width="***" height="***">',
+    a: "width=250, height=250",
+    b: "width=400, height=250",
+    c: "width=250, height=400",
+    d: "width=400, height=400",
+    correct: "c",
+  },
+];
